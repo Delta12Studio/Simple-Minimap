ea3494c50103cadfbb7493edc53900befc518bd8
Delta12 Studio <84870805+Delta12Studio@users.noreply.github.com>
2021-11-10 14:20:03 -0300

    Up

---
+++ Player/Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var ACCELERATION = 500
+var MAX_SPEED = 80
+var FRICTION = 500
+var velocity = Vector2.ZERO
+var direction = Vector2.DOWN
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _process(delta):
+
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		direction = input_vector
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	move()
+
+func move():
+	velocity = move_and_slide(velocity)
